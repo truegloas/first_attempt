842de1a89ed3d5b47a7702ce308ed9667a2d4c83
truegloas <gloasrow@gmail.com>
2024-07-06 19:24:45 +0700

    learning funcs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+fn view_string(s: &str) {
+    print!("{s}");
+}
+
+fn view_number(num: i32) {
+    print!("{num}");
+}
+
 fn main() {
-    println!("Hello, world!");
+    let s: &str = "Hi, gloas";
+
+    let num = 8;
+
+    view_string(s);
+    println!("");
+    view_number(num);
 }
