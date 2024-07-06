6a015523d4b3cec78f35908da29f7df7f2f3d0f2
truegloas <gloasrow@gmail.com>
2024-07-06 23:06:22 +0700

    Add first operation (sum)

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,14 @@ fn view_number(num: i32) {
     print!("{num}");
 }
 
+fn sum(num1: i32, num2: i32) -> i32 {
+    return num1 + num2;
+}
+
 fn main() {
     let s: &str = "Hi, gloas";
 
-    let num = 8;
+    let num = sum(5, -10);
 
     view_string(s);
     println!("");
