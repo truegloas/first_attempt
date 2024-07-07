cffde8997b7fd9a18b32664a9dec62eb78c0e501
truegloas <gloasrow@gmail.com>
2024-07-08 00:53:34 +0700

    first_program

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,12 @@
-fn view_string(s: &str) {
-    print!("{s}");
-}
+use std::io;
 
-fn view_number(num: i32) {
-    print!("{num}");
-}
+fn main() -> io::Result<()> {
+    println!("Enter your name: ");
 
-fn sum(num1: i32, num2: i32) -> i32 {
-    return num1 + num2;
-}
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer)?;
 
-fn view_array(num_array:[i32; 8]) {
-    for i in num_array {
-        print!("{i} ");
-    }
-}
+    println!("Hello, {}", buffer);
 
-fn main() {
-    let s: &str = "Hi, gloas";
-
-    let num = sum(5, -10);
-
-    view_string(s);
-    println!("");
-    view_number(num);
-
-    println!("");
-
-    let mut numbers = [134, 235645, 65643, 643214, 53412, 6234, 7234, 81342];
-    println!("{:?}", numbers); // [1, 2, 3, 4, 5, 6, 7, 8],
-    println!("ты гей {}", numbers[3]);
-    numbers[3] = 33122i32;
-
-    for i in 1..10 {
-        view_number(i);
-        print!(" ");
-    }
-
-    println!();
-    view_array(numbers);
-    println!();
+    Ok(())
 }
