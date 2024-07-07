c34bbf6cfa8894b0abeeea28eb2cc4e9a96a966f
truegloas <gloasrow@gmail.com>
2024-07-08 06:36:15 +0700

    Just don't watch cringe guides for Rust anymore

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-fn sum(num1: i8, num2: i8) {
-    num1 + num2;
+fn sum(num1: i8, num2: i8) -> i8 {
+    return num1 + num2;
 }
 
 fn main() {
-    println!("Hello, World");
+    println!("{}", sum(5, 8));
 }
