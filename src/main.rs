a1e5b2e707d0d3b8959c67e0e37c1f7f89978648
truegloas <gloasrow@gmail.com>
2024-07-29 14:28:42 +0700

    Some cringe attempts to shadoww

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,23 @@ fn var_and_mut_lesson() {
     println!("Const + immut: {}", CONST_VARIABLE + immutable_variable);
     println!("Const + mut: {}", CONST_VARIABLE + mutable_variable);
     println!("Immut + mut: {}", immutable_variable + mutable_variable);
+    println!("Shadowed variable: {}", shadowing_lesson(50));
+    println!("Shadowed division: {}", shadowing_lesson(25));
 }
 
 fn main() {
     var_and_mut_lesson();
+}
+
+fn shadowing_lesson(x: i32) -> i32 {
+    {
+        let x = x * 2;
+        if x == 50 {
+            return x / 2;
+        } else {
+            println!("{}", x * 2);
+        }
+    }
+
+    x
 }
