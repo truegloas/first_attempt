b6a646e1261563121150ab1ca6b388822cb69ef0
truegloas <gloasrow@gmail.com>
2024-08-07 10:10:08 +0700

    Data Types Lesson - Scalar Types

---
+++ src/data_types.rs
@@ -0,0 +1,6 @@
+// number after a type means amount of bits allocated in memory
+
+pub const INTEGER_CONST_VAR: i8 = -128;
+pub const UNSIGNED_INT_CONST: u8 = 255;
+// There is just two types for floating point number 32 and 64 bit (f32, f64)
+pub const PI: f32 = 3.1415926535;
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
-mod lib;
-use lib::*;
+mod const_variable;
+mod data_types;
+use const_variable::*;
+use data_types::*;
+
+fn main() {
+    println!("Variables and Mutability Lesson");
+    var_and_mut_lesson();
+
+    println!("\nShadowing Lesson");
+    shadowing_lesson();
+
+    println!("\nData Types Lesson");
+    data_types_lesson();
+}
 
 fn var_and_mut_lesson() {
     let immutable_variable: i32 = 20;
@@ -10,21 +23,16 @@ fn var_and_mut_lesson() {
     mutable_variable += 10;
 
     let new_immut = mutable_variable * 10 - 5;
+    // const NEW_CONST: i32 = new_immut - 1000 * 20 - 1000;
 
     println!("{} {} {}", CONST_VARIABLE, immutable_variable, mutable_variable);
     println!("Const + immut: {}", CONST_VARIABLE + immutable_variable);
     println!("Const + mut: {}", CONST_VARIABLE + mutable_variable);
     println!("Immut + mut: {}", immutable_variable + mutable_variable);
-    println!("Shadowed variable: {}", shadowing_lesson(50));
-    println!("Shadowed division: {}", shadowing_lesson(25));
     println!("New immutable: {}", new_immut);
 }
 
-fn main() {
-    var_and_mut_lesson();
-}
-
-fn shadowing_lesson(x: i32) -> i32 {
+fn shadowing_example(x: i32) -> i32 {
     {
         let x = x * 2;
         if x == 50 {
@@ -35,4 +43,15 @@ fn shadowing_lesson(x: i32) -> i32 {
     }
 
     x
+}
+
+fn shadowing_lesson() {
+    println!("Shadowed variable: {}", shadowing_example(50));
+    println!("Shadowed division: {}", shadowing_example(25));
+}
+
+fn data_types_lesson(){
+    println!("Floating Point Number {PI}");
+    println!("Integer Number {INTEGER_CONST_VAR}");
+    println!("Unsigned number: {UNSIGNED_INT_CONST}");
 }
